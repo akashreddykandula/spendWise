48fa6700ba1d9df0868089327cf0094e234c0bed
akashreddykandula <akashreddykandula@gmail.com>
2025-12-30 20:25:14 +0530

    Fix baseurl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://spendwise-backend-j54p.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
