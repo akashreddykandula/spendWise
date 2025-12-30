d5e545e1a257607ff3abafdd013f124f02ae2132
akashreddykandula <akashreddykandula@gmail.com>
2025-12-30 18:38:26 +0530

    fix cors for vercel frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,13 @@ connectDB ();
 
 const app = express ();
 
-app.use (cors ());
+app.use (
+  cors ({
+    origin: ['http://localhost:5173', 'https://spendwise.vercel.app'],
+    credentials: true,
+  })
+);
+
 app.use (express.json ());
 
 app.use ('/api/auth', authRoutes);
