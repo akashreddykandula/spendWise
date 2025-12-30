fec55e8efa0122e4c910ac1f8840c91c72f0101c
akashreddykandula <akashreddykandula@gmail.com>
2025-12-30 22:12:32 +0530

    placed cors in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,28 @@ import reportRoutes from './routes/reportRoutes.js';
 
 const app = express ();
 
-app.use (cors ());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://spend-wise-gamma-two.vercel.app',
+  'https://spend-wise-opal.vercel.app',
+];
+
+app.use (
+  cors ({
+    origin: function (origin, callback) {
+      // allow Postman / server-to-server
+      if (!origin) return callback (null, true);
+
+      if (allowedOrigins.includes (origin)) {
+        callback (null, true);
+      } else {
+        callback (new Error ('CORS not allowed'));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use (helmet ());
 app.use (express.json ());
 app.use (morgan ('dev'));
